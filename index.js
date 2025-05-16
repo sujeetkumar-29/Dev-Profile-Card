fb057f3d2b43a84e0372a824fac83528cc0aeb70
sujeet kumar <sujeetkr503@gmail.com>
2025-05-16 13:14:30 +0530

    index

--- index.js
+++ index.js
@@ -21,17 +21,34 @@ const db = mysql.createConnection({
   connectionLimit: 10,
   queueLimit: 0
 });
-  db.connect(err => {
-    if (err) {
-      console.error("Database connection error:", err);
-      return;
-    }
-    console.log("Connected to database!");
-  });
-  db.query('SELECT * FROM users', (err, results) => {
-  if (err) throw err;
-  console.log(results);
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
+  // db.connect(err => {
+  //   if (err) {
+  //     console.error("Database connection error:", err);
+  //     return;
+  //   }
+  //   console.log("Connected to database!");
+  // });
+  // pool.connect(err => {
+  //   if (err) {
+  //     console.error("Database connection error:", err);
+  //     return;
+  //   }
+  //   console.log("Connected to database!");
+  // });
+//   pool.query('SELECT * FROM users', (err, results) => {
+//   if (err) throw err;
+//   console.log(results);
+// });
 // try{
 //   connection.query("SHOW TABLES",(err,results)=>{
 //     if(err) throw err;
