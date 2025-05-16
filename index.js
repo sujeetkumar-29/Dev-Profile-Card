057c4abca23c62a852572aedd3221435dd216a1c
sujeet kumar <sujeetkr503@gmail.com>
2025-05-16 11:43:04 +0530

    added gitignore

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const dotenv = require("dotenv").config();
 const { v4: uuidv4 } = require("uuid");
 const mysql = require("mysql2");
 const express = require("express");
@@ -11,11 +12,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Database connection
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    database: "developer_profiles",
-    password: process.env.pass,
-  });
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
   db.connect(err => {
     if (err) {
       console.error("Database connection error:", err);
